0061a813779fc59db59a3c3bcb93008f209b14fa
Roberto <bob@Robertos-MacBook-Pro.local>
2024-07-01 14:34:22 -0500

    feat: task1 - aswers

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,31 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+import pandas as pd
+from collections import defaultdict, Counter
+import time
+from memory_profiler import profile
 
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    
+    date_counts = Counter()
+    user_counts = defaultdict(lambda: defaultdict(int))
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            tweet = json.loads(line)
+            date_only = datetime.strptime(tweet['date'], '%Y-%m-%dT%H:%M:%S%z').date()
+            username = tweet['user']['username']
+            
+            date_counts[date_only] += 1
+            user_counts[date_only][username] += 1
+    
+    top_dates = date_counts.most_common(10)
+    
+    result = []
+    for date, _ in top_dates:
+        top_user = max(user_counts[date], key=user_counts[date].get)
+        result.append((date, top_user))
+    
+    return result
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,27 @@
 from typing import List, Tuple
+import json
+import pandas as pd
 from datetime import datetime
+from memory_profiler import profile
 
+@profile
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(json.loads(line))
+
+    df = pd.json_normalize(data)
+    
+    df['date_only'] = pd.to_datetime(df['date']).dt.date
+    
+    top_dates = df['date_only'].value_counts().nlargest(10).index
+    
+    result = []
+    for date in top_dates:
+        date_df = df[df['date_only'] == date]
+        top_user = date_df['user.username'].value_counts().idxmax()
+        result.append((date,  top_user))
+    
+    return result
